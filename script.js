c59d2b5619964332b3ae95add08a944bfcfd5229
srujana-porandla <131435996+srujana-porandla@users.noreply.github.com>
2024-06-20 20:44:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let counterB=document.getElementById("counter");
+let incremetB=document.getElementById("incrementBtn");
+
+let counterValue=0;
+
+incrementB.addListener("click",()=>{
+	console.log(`${counterValue}`);
+	 counterValue++;
+})
